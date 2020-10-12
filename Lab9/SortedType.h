54b0b2e64b52f82cdb7184df584eaa2a061b066c
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2020-10-13 03:34:27 +0600

    Add files via upload

---
+++ Lab9/SortedType.h
@@ -0,0 +1,33 @@
+#ifndef SORTEDTYPE_H_INCLUDED
+#define SORTEDTYPE_H_INCLUDED
+
+template <class ItemType>
+class SortedType
+{
+    struct NodeType
+    {
+        ItemType info;
+        NodeType* next;
+    };
+public:
+    SortedType();
+    ~SortedType();
+    bool IsFull();
+    int LengthIs();
+    void MakeEmpty();
+    void RetrieveItem(ItemType&,bool&);
+    void InsertItem(ItemType);
+    void DeleteItem(ItemType);
+    void ResetList();
+    void GetNextItem(ItemType&);
+    void Display();
+    void NextPossibleEntries(ItemType, ItemType);
+private:
+    NodeType* listData;
+    int length;
+    NodeType* currentPos;
+
+};
+
+#endif // SORTEDTYPE_H_INCLUDED
+
---
+++ Lab9/main.cpp
@@ -0,0 +1,73 @@
+#include "SortedType.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SortedType<int> s;
+    int n;
+    cout<<"The length of the list "<<s.LengthIs()<<endl;
+
+    s.InsertItem(5);
+    s.InsertItem(7);
+    s.InsertItem(4);
+    s.InsertItem(2);
+    s.InsertItem(1);
+
+
+     cout<<"The length of the list "<<s.LengthIs()<<endl;
+
+     s.ResetList();
+     for(int i=0; i< s.LengthIs(); i++)
+     {
+         s.GetNextItem(n);
+         cout<< n <<endl;
+     }
+
+        bool found;
+         n=6;
+
+     s.RetrieveItem(n,found);
+     if(found == true)
+         cout<<"item is found"<<endl;
+     else
+         cout<<"item is not found"<<endl;
+         n=5;
+
+     s.RetrieveItem(n,found);
+     if(found == true)
+         cout<<"item is found"<<endl;
+     else
+         cout<<"item is not found"<<endl;
+
+         if(s.IsFull())
+        cout<<"List is full"<<endl;
+     else
+        cout<<"List is not full"<<endl;
+
+     s.DeleteItem(1);
+     n=1;
+
+     s.RetrieveItem(n,found);
+     if(found == true)
+         cout<<"item is found"<<endl;
+     else
+         cout<<"item is not found"<<endl;
+         s.ResetList();
+     for(int i=0; i< s.LengthIs(); i++)
+     {
+         s.GetNextItem(n);
+         cout<< n <<endl;
+     }
+
+     if(s.IsFull())
+        cout<<"List is full"<<endl;
+     else
+        cout<<"List is not full"<<endl;
+
+        cout<<"The length of the list "<<s.LengthIs()<<endl;
+
+
+    return 0;
+}
